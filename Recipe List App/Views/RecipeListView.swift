ea29e97133a53ae84c846986493c225928ae4a7d
Inarius4 <elskedevries@gmail.com>
2023-06-25 16:27:27 +0200

    Environment object added

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct RecipeListView: View {
     
-    // Reference the view model
-    @ObservedObject var model = RecipeModel()
+    @EnvironmentObject var model:RecipeModel
     
     var body: some View {
         
@@ -42,5 +41,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
